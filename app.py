1cea162415c727de3e6339e951e9845ac8ad1e28
nomah98 <noahmrochefort@gmail.com>
2019-07-24 15:41:39 -0400

    setup

---
+++ app.py
@@ -0,0 +1,21 @@
+import requests
+import os
+import json
+import queue
+from flask import Flask, request
+from urllib.parse import urlencode
+from urllib.request import Request, urlopen
+from collections import OrderedDict
+
+app = Flask(__name__)
+
+@app.route('/thirtytwobot', methods=['GET'])
+def index():
+	print('JuliaPakey')
+	return 'https://www.instagram.com/p/BJmB0B4gLQO/'
+
+@app.route('/', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    sent = str(data['text'])
+    return data['user_id']

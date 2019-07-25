a74e95689e54d4ea65411607dc1f115981ed4414
nomah98 <noahmrochefort@gmail.com>
2019-07-25 10:33:31 -0400

    setup

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def index():
     return 'https://www.instagram.com/p/BJmB0B4gLQO/'
 
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['DELETE'])
 def webhook():
     data = request.get_json()
     #group.memberships.remove('156400982779367387')
@@ -28,6 +28,6 @@ def webhook():
 #@app.route('/https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove?=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h', methods=['POST'])
 def removeTom():
     #group.memberships.remove('156400982779367387')
-    post_url = 'https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove'
+    post_url = 'https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove?token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h'
     response = urllib.request.urlopen(post_url, {})
     return 'ok'

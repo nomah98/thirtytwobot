071abad0558cbbb0a72b69206bc3a5f04fc819eb
nomah98 <noahmrochefort@gmail.com>
2019-07-25 12:21:29 -0400

    setup

--- app.py
+++ app.py
@@ -33,6 +33,6 @@ def groups():
 #@app.route('/https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove?=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h', methods=['POST'])
 def removeTom():
     #group.memberships.remove('156400982779367387')
-    post_url = 'https://api.groupme.com/v3/groups/39105660/members/459607275/remove?token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h/'
+    post_url = 'https://api.groupme.com/v3/groups/39105660/members/459607275/remove?token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h'
     response = urllib.request.urlopen(post_url, {})
     return response

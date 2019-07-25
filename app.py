e36c4136b861a0e28356895bd5cf56fa2b365a2f
nomah98 <noahmrochefort@gmail.com>
2019-07-25 10:46:42 -0400

    setup

--- app.py
+++ app.py
@@ -14,20 +14,19 @@ app = Flask(__name__)
 
 @app.route('/thirtytwobot', methods=['GET'])
 def index():
-    print('JuliaPakey 2')
+    print('JuliaPakey')
     return 'https://www.instagram.com/p/BJmB0B4gLQO/'
 
 
-@app.route('/', methods=['DELETE'])
+@app.route('/', methods=['POST'])
 def webhook():
     data = request.get_json()
-    #group.memberships.remove('156400982779367387')
     removeTom()
     return 'ok', 200
 
 #@app.route('/https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove?=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h', methods=['POST'])
 def removeTom():
     #group.memberships.remove('156400982779367387')
-    post_url = 'https://api.groupme.com/v3/groups/39105660/members/156400982779367387?client_id=3cefe43bef5d04bd22d3958597&token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h'
+    post_url = 'https://api.groupme.com/v3/groups/39105660/members/156400982779367387?token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h'
     response = urllib.request.urlopen(post_url, {})
     return 'ok'

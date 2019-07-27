ccfb30b2d0e73bcada47f9a704c7d2354c48e508
nomah98 <noahmrochefort@gmail.com>
2019-07-27 09:19:34 -0400

    setup

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
 from flask import Flask, request
 from datetime import date
-
+import requests
 import urllib.request
+import os
 import groupy
 from groupy import Client
 import groupy.api
 
 app = Flask(__name__)
-
+app.config['apiToken'] = os.environ['apiToken']
+app.config['thirtyTwoBotID'] = os.environ['thirtyTwoBotID']
 
 
 @app.route('/thirtytwobot', methods=['GET'])
@@ -21,8 +23,8 @@ def webhook():
     data = request.get_json()
     if data['text'] == '\larosa':
         larosaCounter()
-    #print(data['members']['id'])
-    removeTom()
+    if data['user_id'] == '36815098':
+        removeTom()
     return 'ok'
 
 
@@ -32,16 +34,24 @@ def groups():
     print(info)
     return info
 
-
-#@app.route('/https://api.groupme.com/v3/groups/39105660/members/156400982779367387/remove?=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h', methods=['POST'])
 def removeTom():
-    #group.memberships.remove('156400982779367387')
-    post_url = 'https://api.groupme.com/v3/groups/39105660/members/459607275/remove?token=RFB9t35ct1lA7wHOdbBNZpJKeEqiDTPCGz5nwN5h'
+    post_url = 'https://api.groupme.com/v3/groups/39105660/members/459607275/remove?token=' + os.environ['apiToken']
     response = urllib.request.urlopen(post_url, {})
     return response
 
 def larosaCounter():
     death = date(2019, 3, 4)
-    daysSince = death - date.today()
+    daysSince = str(death - date.today()) + ' days since Larosa died'
     print(daysSince)
+    sendMessage(daysSince)
     return daysSince
+
+
+def sendMessage(msg):
+    url = 'https://api.groupme.com/v3/bots/post'
+    payload = {
+        'bot_id'	: os.environ['thirtyTwoBotID'],
+        'text'		: msg
+    }
+    response = requests.post(url, data=payload)
+    return payload

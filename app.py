0d90d902817831df7f4d5cec295425d3f9e6dc80
nomah98 <noahmrochefort@gmail.com>
2019-07-27 11:21:27 -0400

    setup

--- app.py
+++ app.py
@@ -25,6 +25,8 @@ def webhook():
         larosaCounter()
     if data['user_id'] == '36815098':
         removeTom()
+    if data['text'] == 'luc':
+        lucBot()
     return 'ok'
 
 
@@ -42,9 +44,20 @@ def removeTom():
 def larosaCounter():
     death = date(2019, 3, 4)
     daysSince = str((date.today() - death).days) + ' days since Larosa died'
-    print(daysSince)
     sendMessage(daysSince)
-    return daysSince
+
+
+def lucBot():
+    url = 'https://api.instagram.com/oauth/access_token'
+    payload = {
+    'client_id': 'fd94c8a2662e472aa3b203184e915ec6',
+    'client_secret': '4271ddc1006b48d19e14a4419a8f773a',
+    'grant_type' : 'authorization_code',
+    'redirect_uri' : 'https://glacial-garden-64910.herokuapp.com/',
+    'code' : '10616b10653947238878e74ced4b4b74'
+    }
+    response = requests.post(url, data=payload)
+    return payload
 
 
 def sendMessage(msg):

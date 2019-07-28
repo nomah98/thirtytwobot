194f445c7a305aaffd130f7e0a0937f481509c84
nomah98 <noahmrochefort@gmail.com>
2019-07-28 14:40:04 -0400

    setup

--- app.py
+++ app.py
@@ -47,23 +47,10 @@ def larosaCounter():
     daysSince = str((date.today() - death).days) + ' days since Larosa died'
     sendMessage(daysSince)
 
-juliaDict = {
-        0: removeTom(),
-        1: removeTom(),
-        2: removeTom(),
-        3: sendMessage('https://i.groupme.com/1080x1350.jpeg.c036fbed65a144b98320174c32eb73bf'),
-        4: sendMessage('https://i.groupme.com/1080x1241.jpeg.eb9b0971005c4e1f89ea83808d26d278'),
-        5: sendMessage('https://i.groupme.com/1080x1350.jpeg.ca6be17203af4796a43fa00b42f72c5d'),
-        6: sendMessage('https://i.groupme.com/1080x1350.jpeg.b6be3125d82442f7b98fe80bf88da226'),
-        7: sendMessage('https://i.groupme.com/1080x1350.jpeg.3a7249d893f44746aac47ccbe36ebb4c'),
-        8: sendMessage('https://i.groupme.com/1080x1221.jpeg.64c1720db16f41d782848bc689bc8a80'),
-        9: removeTom(),
-        10: removeTom(),
-    }
-
 def lucBot():
     number = random.random() * 10
-    juliaDict.get(number)
+    str(number)
+    funcDict(number)
 
 def sendMessage(msg):
     url = 'https://api.groupme.com/v3/bots/post'
@@ -73,3 +60,20 @@ def sendMessage(msg):
     }
     response = requests.post(url, data=payload)
     return payload
+
+def funcDict(num):
+    juliaDict = {
+        "0": sendMessage('https://i.groupme.com/1080x1350.jpeg.c036fbed65a144b98320174c32eb73bf'),
+        "1": removeTom(),
+        "2": removeTom(),
+        "3": sendMessage('https://i.groupme.com/1080x1350.jpeg.c036fbed65a144b98320174c32eb73bf'),
+        "4": sendMessage('https://i.groupme.com/1080x1241.jpeg.eb9b0971005c4e1f89ea83808d26d278'),
+        "5": sendMessage('https://i.groupme.com/1080x1350.jpeg.ca6be17203af4796a43fa00b42f72c5d'),
+        "6": sendMessage('https://i.groupme.com/1080x1350.jpeg.b6be3125d82442f7b98fe80bf88da226'),
+        "7": sendMessage('https://i.groupme.com/1080x1350.jpeg.3a7249d893f44746aac47ccbe36ebb4c'),
+        "8": sendMessage('https://i.groupme.com/1080x1221.jpeg.64c1720db16f41d782848bc689bc8a80'),
+        "9": removeTom(),
+        "10": removeTom(),
+    }
+    juliaDict.get(num)
+

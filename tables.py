90510a48841efb79e2cb33d8dbed01ccf3caa1dc
nomah98 <noahmrochefort@gmail.com>
2019-08-02 12:19:33 -0400

    setup

--- tables.py
+++ tables.py
@@ -9,6 +9,5 @@ class Roommate(db.Model):
 
 class Insult(db.Model):
     __tablename__ = 'insults'
-    insultID = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(), ForeignKey('roommates.name'))
-    insult = db.Column(db.String())
+    insult = db.Column(db.String(), primary_key=True)

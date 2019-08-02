3a730e2f9fe93dfd9f5613f14d7b43cfbe92178d
nomah98 <noahmrochefort@gmail.com>
2019-08-02 12:40:27 -0400

    setup

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import os
 import random
 from flask_sqlalchemy import SQLAlchemy
 import pymysql
+from sqlalchemy import sql
 
 app = Flask(__name__)
 app.config['apiToken'] = os.environ['apiToken']
@@ -107,7 +108,7 @@ def urban(term):
 
 
 def roastBot(message):
-    insultList = Insult.query.filter(str(message[1]))
+    insultList = Insult.query.filter(sql.text(message[1]))
     print(insultList)
     return insultList
 

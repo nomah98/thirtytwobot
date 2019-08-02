81cf900a0677b4e46a209fa430c972e83b6574b3
nomah98 <noahmrochefort@gmail.com>
2019-08-02 11:34:23 -0400

    setup

---
+++ tables.py
@@ -0,0 +1,13 @@
+from app import db
+from sqlalchemy import Table, Column, Integer, ForeignKey
+
+
+class Roommate(db.Model):
+    __tablename__ = 'roommates'
+    name = db.Column(db.String(), primary_key=True)
+
+
+class Insult(db.Model):
+    __tablename__ = 'insults'
+    name = db.Column(db.String(), ForeignKey('roommates.name'))
+    insult = db.Column(db.String())

4db307315ca65bd7536d35bc0d0f2d309490b663
nomah98 <noahmrochefort@gmail.com>
2019-08-04 18:37:11 -0400

    setup

--- app.py
+++ app.py
@@ -108,7 +108,7 @@ def urban(term):
 
 
 def roastBot(message):
-    sesh = tables.DBSession.query(Insult).filter(name='Noah')
+    sesh = tables.DBSession.query(Insult).filter_by(name='Noah')
     print(sesh)
     sendMessage(str(sesh.name))
     return sesh

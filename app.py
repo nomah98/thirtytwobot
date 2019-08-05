f5164fef2e119087bda4530f2e4787c3d7495a1d
nomah98 <noahmrochefort@gmail.com>
2019-08-05 14:38:57 -0400

    setup

--- app.py
+++ app.py
@@ -111,7 +111,7 @@ def sendMessage2(msg):
     attachmentList = "[{'loci':[[3, 5]], 'type': 'mentions', 'user_ids:' ['37983222']}]"
     url = 'https://api.groupme.com/v3/bots/post'
     payload = {
-        'attachments': [{"loci": [[3, 5]], "type": "mentions", "user_ids": ['37983222']}],
+        'attachments': [{"loci": [[3, 5]], "type": "mention", "user_ids": ['37983222']}],
         'bot_id'	: '3cefe43bef5d04bd22d3958597',
         'text'		: msg
     }

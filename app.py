aad0579b3428599d4a76537390272df997e95d70
Noah M Rochefort <noahmrochefort@gmail.com>
2020-04-03 16:28:37 -0400

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ def webhook():
         addRoast(sentMessage)
     if command == '\\addroastee':
         addRoastee(sentMessage)
-    if command == '\\trash':
+    if command == '\\blackout':
         pickRoommate()
     return 'ok'
 
@@ -161,7 +161,7 @@ def sendRoomie(ID, name):
     nameLength = len(name) + 1
     payload = {
         'bot_id' : os.environ['thirtyTwoBotID'],
-        'text' : 'It is @' + name + 's turn to take out the trash',
+        'text' : 'It is @' + name + 's turn to blackout tonight',
         'attachments' : [
             {
                 'type': 'mentions',
